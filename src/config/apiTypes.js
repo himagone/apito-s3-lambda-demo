ce92ca14e0de790038c5965abfc6070781c6b448
himagone <>
2024-08-23 13:41:34 +0900

    add

---
+++ src/config/apiTypes.js
@@ -0,0 +1,130 @@
+module.exports = [
+  {
+    dataname: 'EvacuationInformationSediment',
+    entityId: 'Place'
+  },
+  {
+    dataname: 'EvacuationInformationFlood',
+    entityId: 'Place'
+  },
+  {
+    dataname: 'SewerFloodRiskArea',
+    entityId: 'Place'
+  },
+  {
+    dataname: 'DebrisFlowVigilanceArea',
+    entityId: 'Place'
+  },
+  {
+    dataname: 'DebrisFlowSpecialVigilanceArea',
+    entityId: 'Place'
+  },
+  {
+    dataname: 'RestrictedTrafficAreaInformation',
+    entityId: 'RestrictedTrafficArea'
+  },
+  {
+    dataname: 'BroadcastRadioChildStation',
+    entityId: 'BroadcastRadioChildStation'
+  },
+  {
+    dataname: 'WeatherForecast',
+    entityId: 'WeatherForecast'
+  },
+  {
+    dataname: 'TsunamiEvacuationBuilding',
+    entityId: 'TsunamiEvacuationBuilding'
+  },
+  {
+    dataname: 'EvacuationSpace',
+    entityId: 'EvacuationSpace'
+  },
+  {
+    dataname: 'LandslidePreventionArea',
+    entityId: 'Place'
+  },
+  {
+    dataname: 'LandslideVigilanceArea',
+    entityId: 'Place'
+  },
+  {
+    dataname: 'TsunamiFloodRiskArea',
+    entityId: 'Place'
+  },
+  {
+    dataname: 'FloodRiskAreaMaxTime',
+    entityId: 'Place'
+  },
+  {
+    dataname: 'FloodRiskAreaMaxScale',
+    entityId: 'Place'
+  },
+  {
+    dataname: 'FloodRiskAreaPlanScale',
+    entityId: 'Place'
+  },
+  {
+    dataname: 'HouseCollapseRiskAreaOverflowing',
+    entityId: 'Place'
+  },
+  {
+    dataname: 'HouseCollapseRiskAreaRiverErosion',
+    entityId: 'Place'
+  },
+  {
+    dataname: 'WeatherAlert',
+    entityId: 'WeatherAlert'
+  },
+  {
+    dataname: 'ReliefHospital',
+    entityId: 'MedicalFacility'
+  },
+  {
+    dataname: 'FirstAidStation',
+    entityId: 'MedicalFacility'
+  },
+  {
+    dataname: 'UnderpassInformation',
+    entityId: 'UnderpassInformation'
+  },
+  {
+    dataname: 'StreamGauge',
+    entityId: 'WeatherObserved'
+  },
+  {
+    dataname: 'CameraInformation',
+    entityId: 'CameraInformation'
+  },
+  {
+    dataname: 'SteepSlopeFailureVigilanceArea',
+    entityId: 'Place'
+  },
+  {
+    dataname: 'SteepSlopeFailureSpecialVigilanceArea',
+    entityId: 'Place'
+  },
+  {
+    dataname: 'PrecipitationGauge',
+    entityId: 'WeatherObserved'
+  },
+  {
+    dataname: 'DrinkingWaterTank',
+    entityId: 'DrinkingWaterTank'
+  },
+  {
+    dataname: 'Aed',
+    entityId: 'Aed'
+  },
+  {
+    dataname: 'SandbagStation',
+    entityId: 'SandbagStation'
+  },
+  {
+    dataname: 'FloodPreventionWarehouse',
+    entityId: 'Warehouse'
+  },
+  {
+    dataname: 'EvacuationShelter',
+    entityId: 'EvacuationShelter'
+  }
+];
--- src/handlers/apiHandler.js
+++ src/handlers/apiHandler.js
@@ -2,40 +2,56 @@ const axios = require('axios');
 const AWS = require('aws-sdk');
 const moment = require('moment');
 const s3 = new AWS.S3();
+const apiTypes = require('../config/apiTypes.js');
 
+async function callApiAndSaveToS3(apiType){
+  const headers = {
+    'fiware-service': FIWARE_SERVICE,
+    'fiware-servicepath': '/' + apiType.dataname,
+    'x-request-trace-id': process.env.REQUEST_TRACE_ID,
+    'apikey': API_KEY
+  };
 
-const dataname = 'WeatherAlert';
-const servicepath = '/'+ dataname;
+  const entityId = `jp.smartcity-yaizu.${apiType.entityId}.1`;
+  const param = new URLSearchParams({ type: apiType.name });
+  const requestURL = `${EXTERNAL_API_BASE_URL}${entityId}?${param.toString()}`;
 
-module.exports.callExternalApi = async (event) => {
-  console.log('Environment variables:', process.env);
-  try {
-    const headers = {
-      'fiware-service': process.env.FIWARE_SERVICE,
-      'fiware-servicepath': servicepath,
-      'x-request-trace-id': process.env.REQUEST_TRACE_ID,
-      'apikey': process.env.API_KEY
-    };
-    console.log('Calling external API with headers:', headers);
-    const entityId = 'jp.smartcity-yaizu.'+ dataname+'.1';
-    const param =  new URLSearchParams({
-      type: dataname
-    });
-    requestURL = process.env.EXTERNAL_API_URL + entityId +'?'+param.toString();
-    const response = await axios.get(requestURL, {headers});
+  try{
+    const response = await axios.get(requestURL, { headers });
     const now = moment();
-    console.log('API response:', response.data);
-    const key = `${dataname}/${now.format('YYYY-MM-DD')}/${now.format('HH-mm-ss')}.json`;
+    const fileName = `${apiType.dataname}/${now.format('YYYY-MM-DD')}/${now.format('HH-mm-ss')}.json`;
+
     await s3.putObject({
-      Bucket: process.env.S3_BUCKET,
-      Key: key,
+      Bucket: S3_SERVER_ACCESS_LOGS_USE_BUCKET_POLICY,
+      Key: fileName,
       Body: JSON.stringify(response.data),
       ContentType: 'application/json'
     }).promise();
-
-    return { statusCode: 200, body: JSON.stringify(response.data) };
-  } catch (error) {
-    console.error('Error calling API:', error);
-    return { statusCode: 500, body: JSON.stringify({ error: 'Failed to call external API' }) };
+    console.log(`Successfully saved ${apiType.dataname} to S3.`);
+    return response.data;
+  }catch(error){
+    console.error(`Failed to save ${apiType.dataname} to S3.`, error);
+    return error;
   }
-};
+}
+
+module.exports.handler = async(event)=>{
+  const results = [];
+  const errors = [];
+
+  for(const apiType of apiTypes){
+    try{
+      const result = await callApiAndSaveToS3(apiType);
+      results.push({ apiType: apiType.dataname, status: 'success', data: result });
+    }catch(error){
+      errors.push({ apiType: apiType.dataname, status: 'error', message: error.message });
+    }
+  }
+
+  const response ={
+    statusCode: errors.length > 0 ? 500 : 200,
+    body: JSON.stringify({ results, errors })
+  };
+
+  return response;
+}
