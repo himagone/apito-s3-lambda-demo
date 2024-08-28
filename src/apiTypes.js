d357641eac7f857afff7e278cd8f4c826c49e045
himagone <>
2024-08-28 16:01:40 +0900

    防災のだけに絞る

--- src/apiTypes.js
+++ src/apiTypes.js
@@ -1,151 +1,43 @@
 const apiTypes = [
   {
-    dataname: 'Aed',
-    entityId: 'Aed'
-  },
-  {
-    dataname: 'DrinkingWaterTank',
-    entityId: 'DrinkingWaterTank'
-  },
-  {
-    dataname: 'PrecipitationGauge',
-    entityId: 'WeatherObserved'
-  },
-  {
-    dataname: 'SteepSlopeFailureSpecialVigilanceArea',
-    entityId: 'Place'
-  },
-  {
-    dataname: 'SteepSlopeFailureVigilanceArea',
-    entityId: 'Place'
-  },
-  {
-    dataname: 'CameraInformation',
-    entityId: 'CameraInformation'
-  },
-  {
-    dataname: 'StreamGauge',
-    entityId: 'WeatherObserved'
-  },
-  {
-    dataname: 'UnderpassInformation',
-    entityId: 'UnderpassInformation'
-  },
-  {
-    dataname: 'FirstAidStation',
-    entityId: 'MedicalFacility'
-  },
-  {
-    dataname: 'ReliefHospital',
-    entityId: 'MedicalFacility'
-  },
-  {
-    dataname: 'WeatherAlert',
-    entityId: 'WeatherAlert'
-  },
-  {
-    dataname: 'HouseCollapseRiskAreaRiverErosion',
-    entityId: 'Place'
-  },
-  {
-    dataname: 'HouseCollapseRiskAreaOverflowing',
-    entityId: 'Place'
-  },
-  {
-    dataname: 'FloodRiskAreaPlanScale',
-    entityId: 'Place'
-  },
-  {
-    dataname: 'FloodRiskAreaMaxScale',
-    entityId: 'Place'
-  },
-  {
-    dataname: 'FloodRiskAreaMaxTime',
-    entityId: 'Place'
-  },
-  {
-    dataname: 'TsunamiFloodRiskArea',
-    entityId: 'Place'
-  },
-  {
-    dataname: 'LandslideVigilanceArea',
-    entityId: 'Place'
-  },
-  {
-    dataname: 'LandslidePreventionArea',
-    entityId: 'Place'
-  },
-  {
-    dataname: 'EvacuationSpace',
-    entityId: 'EvacuationSpace'
-  },
-  {
-    dataname: 'TsunamiEvacuationBuilding',
-    entityId: 'TsunamiEvacuationBuilding'
-  },
-  {
-    dataname: 'FloodHistory',
-    entityId: 'Place'
-  },
-  {
-    dataname: 'FloodPreventionWarehouse',
-    entityId: 'Warehouse'
-  },
-  {
-    dataname: 'WeatherForecast',
-    entityId: 'WeatherForecast'
-  },
-  {
-    dataname: 'BroadcastRadioChildStation',
-    entityId: 'BroadcastRadioChildStation'
-  },
-  {
-    dataname: 'RestrictedTrafficAreaInformation',
-    entityId: 'RestrictedTrafficArea'
-  },
-  {
-    dataname: 'DebrisFlowSpecialVigilanceArea',
-    entityId: 'Place'
-  },
-  {
-    dataname: 'DebrisFlowVigilanceArea',
-    entityId: 'Place'
-  },
-  {
-    dataname: 'SandbagStation',
-    entityId: 'SandbagStation'
-  },
-    {
-    dataname: 'SewerFloodRiskArea',
-    entityId: 'Place'
-  },
-  {
-    dataname: 'FirePreventionWaterTank',
-    entityId: 'WaterSupplyForFire'
-  },
-  {
-    dataname: 'EvacuationInformationFlood',
-    entityId: 'Place'
+    dataname: 'EvacuationShelter',
+    entityId: 'EvacuationShelter'
   },
   {
     dataname: 'EvacuationInformationSediment',
     entityId: 'Place'
   },
   {
-    dataname: 'EvacuationShelter',
-    entityId: 'EvacuationShelter'
+    dataname: 'EvacuationInformationFlood',
+    entityId: 'Place'
   },
   {
-    dataname: 'DisasterPreventionWarehouse',
-    entityId: 'Warehouse'
+    dataname: 'UnderpassInformation',
+    entityId: 'UnderpassInformation'
+  },
+  {
+    dataname: 'PrecipitationGauge',
+    entityId: 'WeatherObserved'
+  },
+  {
+    dataname: 'StreamGauge',
+    entityId: 'WeatherObserved'
+  },
+  {
+    dataname: 'WeatherForecast',
+    entityId: 'WeatherForecast'
+  },
+  {
+    dataname: 'WeatherAlert',
+    entityId: 'WeatherAlert'
+  },
+  {
+    dataname: 'RestrictedTrafficAreaInformation',
+    entityId: 'RestrictedTrafficArea'
   },
   {
     dataname: 'DisasterMail',
     entityId: 'DisasterMail'
-  },
-  {
-    dataname: 'DisasterPreventionHeliport',
-    entityId: 'Heliport'
   }
 ];
 
